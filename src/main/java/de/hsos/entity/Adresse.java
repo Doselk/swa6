aebdf077fea904f367f81147e43f1b6945d4d2c6
Doselk <selker.dominik@gmail.com>
2025-05-15 10:32:10 +0200

    dscsd

--- src/main/java/de/hsos/entity/Adresse.java
+++ src/main/java/de/hsos/entity/Adresse.java
@@ -1,4 +1,47 @@
 package de.hsos.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Adresse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String strasse;
+    private String plz;
+    private String ort;
+    private int hausnummer;
+
+    public Adresse() {
+    }
+
+    public Adresse(String strasse, String plz, String ort, int hausnummer) {
+        this.strasse = strasse;
+        this.plz = plz;
+        this.ort = ort;
+        this.hausnummer = hausnummer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public String getPlz() {
+        return plz;
+    }
+
+
+
 }
--- src/main/java/de/hsos/entity/Kunde.java
+++ src/main/java/de/hsos/entity/Kunde.java
@@ -1,4 +1,36 @@
 package de.hsos.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Kunde {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String vorname;
+    private String nachname;
+
+    public Kunde() {
+    }
+
+    public Kunde(String vorname, String nachname) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
 }
